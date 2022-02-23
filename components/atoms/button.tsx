64161a9f27c028eb88586722b0246c24c1d273c8
OceanT-shirt <stylish.trousers@gmail.com>
2022-02-23 19:11:34 +0900

    modified stylesheet

--- components/atoms/button.tsx
+++ components/atoms/button.tsx
@@ -10,12 +10,15 @@ const styles = StyleSheet.create({
         height : 52 ,
         width : 268 ,
         backgroundColor : Colors.dark.colorEmphasis,
+        alignItems: "center",
+        borderRadius: 10,
+        justifyContent: "center",
     },
     buttonTitle: {
-        width : 224,
-        height : 26,
         color : "white",
-        fontSize : 20 ,
+        fontSize : 18 ,
+        alignItems: "center",
+        fontFamily: 'Avenir',
     },
 })
 // ボタンの背景色には、Colors.dark.colorEmphasisを用いる。
@@ -25,7 +28,6 @@ export const CommonButton = (props: ButtonProps) => {
     return(
         <View style={styles.buttonContainer}>
             <Text style={styles.buttonTitle}>
-            {/*    ここに追記*/}
                 { props.title }
             </Text>
         </View>
