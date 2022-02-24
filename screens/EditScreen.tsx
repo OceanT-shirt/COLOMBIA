030989b09e10834539fbdf2dd228e5defd0a6c9f
agate106k <76803256+agate106k@users.noreply.github.com>
2022-02-24 13:12:17 +0900

    Update EditScreen.tsx

--- screens/EditScreen.tsx
+++ screens/EditScreen.tsx
@@ -3,25 +3,51 @@ import { View, StyleSheet } from 'react-native'
 import { Input, Button } from 'react-native-elements'
 import { db,auth } from "../firebase"
 import {updateProfile} from "firebase/auth";
-import firestore,{addDoc, collection, onSnapshot, orderBy, query,where} from 'firebase/firestore';
+import firestore,{updateDoc, collection, doc,onSnapshot, orderBy, query,where} from 'firebase/firestore';
 import {RootTabScreenProps} from "../types";
 import {styles} from "./SignInScreen";
 
 const EditScreen = ({navigation}: RootTabScreenProps<'EditScreen'>) => {
     const [profileList, setProfileList] = useState([])
-    const [displayName,setDisplayName] = useState([])
+    const uid = auth?.currentUser?.uid;
+    const [displayName,setName] = useState([])
     const [photoURL, setURL] = useState([])
     const [profile, setProfile] = useState([])
-    const uid = auth?.currentUser?.uid;
-    const collectionRef = collection(db, "profile", "users", "usersInfo");
-    const q = query(collectionRef,where("uid", "==", uid));
-    useLayoutEffect(() => {
+    const sleep = (waitMsec) => {
+      var startMsec = new Date();
+      while (new Date() - startMsec < waitMsec);
+    }
+    const updateProfiles = () =>{
+    UpdateProfile()
+    updateDoc(
+      doc(
+        db,"profile","users","usersInfo",uid),
+    {
+        uid: uid,
+        info:{
+                  // createdAt: db.timestamp.fromDate(new Date()),
+                  displayName : displayName,
+                  photoURL: photoURL,
+                  profile: profile,
+                  score: 0}
+
+      });
+    }
+
+    useEffect(() => {
+      const collectionRef = collection(db, "profile", "users", "usersInfo");
+      const q = query(collectionRef,where("uid", "==", uid));
       onSnapshot(q, (collectionSnap) => {
       setProfileList(collectionSnap.docs.map(doc => doc.data().info))
       console.log("プロフィール情報の更新を行いました");
       console.log(profileList);
-      console.log(profileList.displayName);
-
+      // console.log(collectionRef.id)
+      // console.log(profileList[0].displayName);
+      // console.log(profileList[0].profile);
+      sleep(1000);
+      setName(profileList[0].displayName);
+      setURL(profileList[0].photoURL);
+      setProfile(profileList[0].profile);
   });
     }, []);
     // const [displayName, setName] = useState([profileList.displayName])
@@ -66,7 +92,7 @@ const EditScreen = ({navigation}: RootTabScreenProps<'EditScreen'>) => {
                     label='profile'
                     leftIcon={{ type: 'material', name: 'face' }}
                     value={profile}
-                    onChangeText={text => setURL(text)}
+                    onChangeText={text => setProfile(text)}
                 />
             {/*<Input*/}
             {/*    placeholder='Enter your Profile'*/}
@@ -76,7 +102,7 @@ const EditScreen = ({navigation}: RootTabScreenProps<'EditScreen'>) => {
             {/*    onChangeText={text => setProfile(text)}*/}
             {/*/>*/}
             </View>
-            <Button title="Update" onPress= {UpdateProfile} buttonStyle={styles.registerButton} containerStyle={styles.buttonContainer} titleStyle={styles.buttonTitle} />
+            <Button title="Update" onPress= {updateProfiles} buttonStyle={styles.registerButton} containerStyle={styles.buttonContainer} titleStyle={styles.buttonTitle} />
         </View>)
 
 }
